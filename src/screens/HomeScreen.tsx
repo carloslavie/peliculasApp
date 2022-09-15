6f1025bf0708e0f6074356ecad4d24884a149d3c
carloslavie <carlosmaria.lavie@nttdata.com>
2022-09-15 15:10:02 -0300

    add carousel

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, ActivityIndicator} from 'react-native';
+import {View, ActivityIndicator, Dimensions} from 'react-native';
 import { useMovies } from '../hooks/useMovies';
+import Carousel from 'react-native-snap-carousel';
 import { MoviePoster } from '../components/MoviePoster';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+const windowWidth = Dimensions.get('window').width;
+// const {width: windowWidth} = Dimensions.get('window');
 
 export const HomeScreen = () => {
     const {peliculasCine, isLoading} = useMovies();
@@ -17,13 +22,20 @@ export const HomeScreen = () => {
         );
     }
 
-    if (peliculasCine){
-        console.log(peliculasCine[2].title);
-    }
-
   return (
     <View style={{marginTop:top + 20}}>
-        {peliculasCine && <MoviePoster movie={peliculasCine[1]}/>}
+        {/* <MoviePoster movie={peliculasCine![1]}/> */}
+
+        <View style={{
+            height: 440,
+        }}>
+            <Carousel
+                data={peliculasCine}
+                renderItem={({item}:any)=><MoviePoster movie={item}/>}
+                sliderWidth={windowWidth}
+                itemWidth={300}
+            />
+        </View>
     </View>
   );
 };
